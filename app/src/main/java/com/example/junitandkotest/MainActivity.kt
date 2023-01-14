4bace8b23f11e1df9adc81418c4a87b99262ecb6
Blackraven93 <reblackraven@gmail.com>
2023-01-14 23:53:20 +0900

    feat(function): add factorialAsString function

--- app/src/main/java/com/example/junitandkotest/MainActivity.kt
+++ app/src/main/java/com/example/junitandkotest/MainActivity.kt
@@ -3,16 +3,24 @@ package com.example.junitandkotest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
 import com.example.junitandkotest.ui.theme.JunitAndKoTestTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -30,14 +38,107 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun Welcome(name: String) {
+    Text(
+        text = stringResource(id = R.string.hello, name),
+        textAlign = TextAlign.Center,
+        style = MaterialTheme.typography.subtitle1
+    )
+}
+
+@Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+}
+
+@Composable
+fun TextAndButton(
+    name: MutableState<String>,
+    nameEntered: MutableState<Boolean>) {
+
+    Row(modifier = Modifier.padding(top = 8.dp)) {
+        TextField(
+            value = name.value,
+            onValueChange = { name.value = it },
+            placeholder = {
+                Text(text = stringResource(id = R.string.hint))
+            },
+            modifier = Modifier
+                .alignByBaseline()
+                .weight(1.0F),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                autoCorrect = false,
+                capitalization = KeyboardCapitalization.Words
+            ),
+            keyboardActions = KeyboardActions(onAny = {
+                nameEntered.value = true
+            })
+        )
+        Button(
+            modifier = Modifier
+                .alignByBaseline()
+                .padding(8.dp),
+            onClick = { nameEntered.value = true }) {
+            Text(text = stringResource(id = R.string.done))
+        }
+    }
+}
+
+@Composable
+fun Hello() {
+    val name = remember { mutableStateOf("") }
+    val nameEntered = remember { mutableStateOf(false) }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment
+            .Center
+    ) {
+        if (nameEntered.value) {
+            Greeting(name.value)
+        } else {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Welcome(name = "Raven")
+                TextAndButton(name, nameEntered)
+            }
+        }
+    }
+}
+
+class HelloProvider: PreviewParameterProvider<String> {
+    override val values: Sequence<String>
+    get() = listOf("PreviewParameterProvider").asSequence()
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JunitAndKoTestTheme {
-        Greeting("Android")
+        Welcome(name = "Raven")
     }
+}
+
+@Composable
+//@Preview(showBackground = true, backgroundColor = 0xffff0000)
+//@Preview(widthDp = 100, heightDp = 100)
+//@Preview(locale = "ko-kr")
+@Preview()
+fun AltGreeting2(@PreviewParameter(HelloProvider::class) name: String) {
+    Hello()
+}
+
+fun factorialAsString(n: Int): String {
+    var result = 1L
+    for (i in 1..n) {
+        result *= i
+    }
+
+    return "$n! = $result"
+}
+
+@Composable
+fun Factorial() {
+
 }
