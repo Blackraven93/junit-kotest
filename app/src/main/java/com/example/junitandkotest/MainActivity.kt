990e08fb22926061db284e32505ce8054d7d4845
Blackraven93 <reblackraven@gmail.com>
2023-02-06 23:54:02 +0900

    feat(view): add row & column cell

--- app/src/main/java/com/example/junitandkotest/MainActivity.kt
+++ app/src/main/java/com/example/junitandkotest/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,6 +41,20 @@ fun CustomImage(image: Int, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun TextCell(text: String, modifier: Modifier = Modifier) {
+    val cellModifier = Modifier
+        .padding(4.dp)
+        .size(100.dp, 100.dp)
+        .border(width = 4.dp, color = Color.Black)
+
+    Text(text = text,
+        cellModifier.then(Modifier),
+        fontSize = 70.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center)
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
@@ -48,20 +63,33 @@ fun DefaultPreview() {
         .border(width = 2.dp, color = Color.Black)
     val secondModifier = Modifier.height(100.dp)
     JunitAndKoTestTheme {
+        Column() {
+            Row(
+                horizontalArrangement = Arrangement.Center,
 
-        Column(
-            Modifier.padding(20.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(
-                "Hello Compose",
-                modifier.then(secondModifier),
-                fontSize = 40.sp,
-                fontWeight = FontWeight.Bold
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            CustomImage(R.drawable.vacation)
+                verticalAlignment = Alignment.CenterVertically
+
+
+            ) {
+                TextCell(text = "1")
+                TextCell(text = "2")
+                TextCell(text = "3")
+            }
+
+            Column(
+                Modifier.padding(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    "Hello Compose",
+                    modifier.then(secondModifier),
+                    fontSize = 40.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                CustomImage(R.drawable.vacation)
+            }
         }
     }
 }
