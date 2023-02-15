3df4441452bb5e704b673d0ef6c82f1a3319dc30
Blackraven93 <reblackraven@gmail.com>
2023-02-15 23:48:17 +0900

    feat(view): create custom Cascadelayout

--- app/src/main/java/com/example/junitandkotest/MainActivity.kt
+++ app/src/main/java/com/example/junitandkotest/MainActivity.kt
@@ -105,6 +105,29 @@ fun DoNotingLayout(
     }
 }
 
+@Composable
+fun CascadeLayout(
+    spacing: Int = 0,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(modifier = modifier, content = content) { measurables, constraints ->
+        var indent = 0
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            var yCoord = 0
+            val placeables = measurables.map { measurable ->
+                measurable.measure(constraints)
+            }
+
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = indent, y = yCoord)
+                indent += placeable.width + spacing
+                yCoord += placeable.height + spacing
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
@@ -117,29 +140,39 @@ fun DefaultPreview() {
             Text("Text Line 4")
         }
 
-        Box(contentAlignment= Alignment.Center,
-            modifier = Modifier.size(120.dp, 80.dp)) {
-            Column {
-                ColorBox(
-                    Modifier
-                        .exampleLayout(0f)
-                        .background(Color.Blue))
-                ColorBox(
-                    Modifier
-                        .exampleLayout(0.25f)
-                        .background(Color.Green))
-                ColorBox(
-                    Modifier
-                        .exampleLayout(0.5f)
-                        .background(Color.Yellow))
-                ColorBox(
-                    Modifier
-                        .exampleLayout(0.25f)
-                        .background(Color.Red))
-                ColorBox(
-                    Modifier
-                        .exampleLayout(0.0f)
-                        .background(Color.Magenta))
+//        Box(contentAlignment= Alignment.Center,
+//            modifier = Modifier.size(120.dp, 80.dp)) {
+//            Column {
+//                ColorBox(
+//                    Modifier
+//                        .exampleLayout(0f)
+//                        .background(Color.Blue))
+//                ColorBox(
+//                    Modifier
+//                        .exampleLayout(0.25f)
+//                        .background(Color.Green))
+//                ColorBox(
+//                    Modifier
+//                        .exampleLayout(0.5f)
+//                        .background(Color.Yellow))
+//                ColorBox(
+//                    Modifier
+//                        .exampleLayout(0.25f)
+//                        .background(Color.Red))
+//                ColorBox(
+//                    Modifier
+//                        .exampleLayout(0.0f)
+//                        .background(Color.Magenta))
+//            }
+//        }
+
+        Box {
+            CascadeLayout(spacing = 20) {
+                Box(modifier = Modifier.size(60.dp).background(Color.Blue))
+                Box(modifier = Modifier.size(80.dp, 40.dp).background(Color.Red))
+                Box(modifier = Modifier.size(90.dp, 100.dp).background(Color.Cyan))
+                Box(modifier = Modifier.size(50.dp).background(Color.Magenta))
+                Box(modifier = Modifier.size(70.dp).background(Color.Green))
             }
         }
     }
